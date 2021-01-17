6696c1aa0f27e7708c8e5b4fede4655d4435a57e
Gianmarco Simone <gianmarcosimone89@gmail.com>
2021-01-17 13:14:31 +0100

    WebGL2 & latest three

--- shaders/msdf.js
+++ shaders/msdf.js
@@ -20,42 +20,60 @@ module.exports = function createMSDFShader (opt) {
     uniforms: {
       opacity: { type: 'f', value: opacity },
       map: { type: 't', value: map || new THREE.Texture() },
-      color: { type: 'c', value: new THREE.Color(color) }
+      color: { type: 'c', value: new THREE.Color(color) },
+      time: { type: "f", value: 1 }
     },
-    vertexShader: [
-      'attribute vec2 uv;',
-      'attribute vec4 position;',
-      'uniform mat4 projectionMatrix;',
-      'uniform mat4 modelViewMatrix;',
-      'varying vec2 vUv;',
-      'void main() {',
-      'vUv = uv;',
-      'gl_Position = projectionMatrix * modelViewMatrix * position;',
-      '}'
-    ].join('\n'),
-    fragmentShader: [
-      '#ifdef GL_OES_standard_derivatives',
-      '#extension GL_OES_standard_derivatives : enable',
-      '#endif',
-      'precision ' + precision + ' float;',
-      'uniform float opacity;',
-      'uniform vec3 color;',
-      'uniform sampler2D map;',
-      'varying vec2 vUv;',
+    vertexShader: `#version 300 es
+      in vec2 uv;
+      in vec2 myUVs;
+      in vec4 position;
 
-      'float median(float r, float g, float b) {',
-      '  return max(min(r, g), min(max(r, g), b));',
-      '}',
+      uniform mat4 projectionMatrix;
+      uniform mat4 modelViewMatrix;
+      
+      out vec2 vUv;
+      out vec2 vMyUVs;
+      
+      void main() { 
+        vUv = uv;
+        vMyUVs = myUVs;
 
-      'void main() {',
-      '  vec3 sample = ' + (negate ? '1.0 - ' : '') + 'texture2D(map, vUv).rgb;',
-      '  float sigDist = median(sample.r, sample.g, sample.b) - 0.5;',
-      '  float alpha = clamp(sigDist/fwidth(sigDist) + 0.5, 0.0, 1.0);',
-      '  gl_FragColor = vec4(color.xyz, alpha * opacity);',
-      alphaTest === 0
-        ? ''
-        : '  if (gl_FragColor.a < ' + alphaTest + ') discard;',
-      '}'
-    ].join('\n')
+        gl_Position = projectionMatrix * modelViewMatrix * position;
+      }
+    `,
+    fragmentShader: `#version 300 es
+      #ifdef GL_OES_standard_derivatives
+      #extension GL_OES_standard_derivatives : enable
+      #endif
+
+      precision ${precision} float;
+      
+      uniform float opacity;
+      uniform vec3 color;
+      uniform sampler2D map;
+      uniform float time;
+      
+      in vec2 vUv;
+      in vec2 vMyUVs;
+      out vec4 myOutputColor;
+
+      float median(float r, float g, float b) {
+        return max(min(r, g), min(max(r, g), b));
+      }
+
+      void main() {
+        vec3 s = ${negate ? '1.0 -' : ''} texture(map, vUv).rgb;
+        float sigDist = median(s.r, s.g, s.b) - 0.5;
+        
+        float alpha = clamp(sigDist/fwidth(sigDist) + 0.5, 0.0, 1.0);
+
+        float y = vMyUVs.y;
+        float t = abs(mod(time, 2.) - 1.);
+        alpha *= 1. - smoothstep(y, y + 0.001, t);
+        
+        myOutputColor = vec4(color.xyz, alpha * opacity);
+        ${alphaTest === 0 ? "" : "if (myOutputColor.a < 0.0001) discard;"}
+      }
+    `
   }, opt);
 };
--- test/test-msdf.js
+++ test/test-msdf.js
@@ -1,17 +1,6 @@
-/*
-  This is an example of 2D rendering, simply
-  using bitmap fonts in orthographic space.
-
-  var geom = createText({
-    multipage: true,
-    ... other options
-  })
- */
-
 global.THREE = require('three')
 var createOrbitViewer = require('three-orbit-viewer')(THREE)
 var shuffle = require('array-shuffle')
-var quotes = shuffle(require('sun-tzu-quotes/quotes.json').join(' ').split('.'))
 var createText = require('../')
 var MSDFShader = require('../shaders/msdf')
 var palettes = require('nice-color-palettes')
@@ -25,7 +14,7 @@ require('./load')({
 
 function start (font, texture) {
   var app = createOrbitViewer({
-    clearColor: background,
+    clearColor: '#000',
     clearAlpha: 1.0,
     fov: 65,
     position: new THREE.Vector3()
@@ -39,17 +28,20 @@ function start (font, texture) {
 
   var container = new THREE.Object3D()
   app.scene.add(container)
-  var count = 25
-  for (var i = 0; i < count; i++) {
-    createGlyph()
-  }
+  createGlyph()
 
   var time = 0
   // update orthographic
   app.on('tick', function (dt) {
     time += dt / 1000
-    var s = (Math.sin(time * 0.5) * 0.5 + 0.5) * 2.0 + 0.5
-    container.scale.set(s, s, s)
+
+    container.children.forEach((child, i) => {
+      child.position.y += Math.sin(time * 2 + i / 4)
+      child.material.uniforms.time.value = time
+    })
+
+
+
     // update camera
     var width = app.engine.width
     var height = app.engine.height
@@ -61,28 +53,43 @@ function start (font, texture) {
   })
 
   function createGlyph () {
-    var angle = (Math.random() * 2 - 1) * Math.PI
+    
+    const text ="Lorem Ipsum"
+
     var geom = createText({
-      text: quotes[Math.floor(Math.random() * quotes.length)].split(/\s+/g).slice(0, 6).join(' '),
+      text,
       font: font,
       align: 'left',
       flipY: texture.flipY
     })
 
+    const chars = text.split('')
+
     var material = new THREE.RawShaderMaterial(MSDFShader({
       map: texture,
       transparent: true,
-      color: palette[Math.floor(Math.random() * palette.length)]
+      color: "white"
     }))
 
     var layout = geom.layout
-    var text = new THREE.Mesh(geom, material)
-    text.position.set(0, -layout.descender + layout.height, 0)
-    text.scale.multiplyScalar(Math.random() * 0.5 + 0.5)
+    
+    const myUVs = new THREE.BufferAttribute(new Float32Array([0, 1, 1, 1, 0, 0, 1, 0]), 2)
+     
+    chars.forEach((_, i) => {
+      const charGeom = createText({
+        text: _,
+        font,
+        align: "left",
+        flipY: texture.flipY
+      })
 
-    var textAnchor = new THREE.Object3D()
-    textAnchor.add(text)
-    textAnchor.rotation.z = angle
-    container.add(textAnchor)
+      charGeom.setAttribute('myUVs', myUVs) 
+      
+      const charMesh = new THREE.Mesh(charGeom, material)
+
+      charMesh.position.set(layout.glyphs[i].position[0], -layout.descender + layout.height, 1)
+      container.add(charMesh)
+    })
+
   }
 }
